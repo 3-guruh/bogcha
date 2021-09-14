ff83a5f1700672715d50078803c7a0ac08f25062
Muminniyoz Shonazarov <muminniyoz2912@gmail.com>
2021-09-14 10:33:07 +0500

    upload

--- src/main/java/uz/itcenter/bogcha/controller/HomeController.java
+++ src/main/java/uz/itcenter/bogcha/controller/HomeController.java
@@ -3,13 +3,27 @@ package uz.itcenter.bogcha.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.Instant;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String boshSahifa(Model model){
+    public String boshSahifa(@CookieValue(name = "ism", defaultValue = "") String ism, Model model, HttpServletResponse res, HttpServletRequest req){
+        System.out.println("Parameter ism: " + ism);
+        Cookie c = new Cookie("salom", "Kalom");
+        c.setMaxAge(90);
+
+        res.addCookie(c);
+        for(Cookie cookie: req.getCookies()){
+            System.out.println(cookie.getName() + " " + cookie.getValue());
+        }
         model.addAttribute("ism", "Sardor");
         return "index";
     }
